bd84be80323590f5f20cdb5edb5cf07700df8d27
Isaac <isaackyuma56@gmail.com>
2025-10-18 22:33:57 +0300

    Add react-hooks-react-router-code-along-v6

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,26 @@
+// ./src/pages/Home.js
 import { useState, useEffect } from "react";
 import UserCard from "../components/UserCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
+      .then((r) => r.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error(error));
   }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>
-  });
+
+  const userList = users.map((user) => (
+    <UserCard key={user.id} user={user} />
+  ));
 
   return (
     <>
       <header>
-        {/* place NavBar here */}
+        <NavBar />
       </header>
       <main>
         <h1>Home!</h1>
@@ -26,6 +28,6 @@ function Home() {
       </main>
     </>
   );
-};
+}
 
 export default Home;
